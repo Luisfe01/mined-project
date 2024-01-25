9b1a78399734f0ff1bc02d7246af6695bf206e69
Luis Garcia <luisflp.garcia@gmail.com>
2024-01-24 19:35:57 -0600

    Cambios 2

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const db = new Sequelize('form_mined', process.env.DBUSER,  process.env.DBPWD, {
-    host: 'localhost',
+    host: process.env.DBHOST,
     dialect: 'mysql'
 });
  
